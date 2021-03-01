f8b31f78fdd9c364314355ec756306903225eadb
Márcio de Jesus <marciohost@gmail.com>
2021-02-28 21:40:53 -0300

    Curso finalizado

--- Intro_ReactJS/3_render/src/App.jsx
+++ Intro_ReactJS/3_render/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+/*
 const listCustomer = [
     {
         id: 1,
@@ -18,22 +18,38 @@ const listCustomer = [
         name: 'Leandro'
     }
 ]
+*/
 
 const App = () => {
-    const renderCustomer = (customer, index) => {
+   /* const renderCustomer = (customer, index) => {
         return (
             <li key={`customer.${customer.id}`}>{customer.name}</li>
         )
+    }*/
+    const showEvent = (e) =>{
+        console.log('Evento clicado');
+        console.log(e);
+        alert(name);
+    }
+    const Button = <button onClick={showEvent}>Mostrar evento</button>
+
+    const name = 'Márcio';
+
+    const handleChange = (e) => {
+        const { value } = e.target;
+        console.log(value);
     }
     return (
         <div>
             <p>Digital Innovation One</p>
             <p>Bem vindo!</p>
-            <div>
+            <input onChange = {handleChange} />
+            {Button}
+            {/* <div>
                 <ul>
                     {listCustomer.map(renderCustomer)}
                 </ul>
-            </div>
+            </div>  */}
         </div >
     );
 };
